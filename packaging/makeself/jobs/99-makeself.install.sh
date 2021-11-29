5445ea2e37bff58cd2a4df5b021eae98a885f9e9
boxjan <i@boxjan.li>
2021-11-29 21:20:07 +0800

    differentiate debug version

--- packaging/makeself/jobs/99-makeself.install.sh
+++ packaging/makeself/jobs/99-makeself.install.sh
@@ -22,6 +22,10 @@ if [ "${VERSION}" == "" ]; then
   exit 1
 fi
 
+if [ ${NETDATA_BUILD_WITH_DEBUG} -eq 1 ]; then
+  VERSION="${VERSION}-debug"
+fi
+
 # -----------------------------------------------------------------------------
 # copy the files needed by makeself installation
 
